d460b14081df1185b8ea79f8d39e104233d4e59c
dsilberschmidt <daniel.silberschmidt@gmail.com>
2021-10-27 18:33:23 +0200

    Hemingway

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -15,6 +15,8 @@ contract Greeter {
         return greeting;
     }
 
+//Hemingway
+
     function setGreeting(string memory _greeting) public {
         console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
         greeting = _greeting;
